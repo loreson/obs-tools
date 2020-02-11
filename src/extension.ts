fe906ccc370729ac5b551c7311c2bb1ef09d5f3f
Christian Voegl <christian.voegl@suse.de>
2020-02-11 12:12:42 +0100

    Can Create local obs builder

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { AsyncResource } from 'async_hooks';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -13,15 +14,80 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('extension.create_OBS_Builder', () => {
 		// The code you place here will be executed every time your command is executed
-
+		create_OBS_Builder(context);
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+
 	});
 
 	context.subscriptions.push(disposable);
 }
 
+async function create_OBS_Builder(context: vscode.ExtensionContext)
+{
+
+	const builder_name = await vscode.window.showInputBox({
+		value: "The builders name (must be unique)",
+		placeHolder: "builder_17",
+		validateInput: text => {
+			//whole line without whitespaces
+			let exp = RegExp('^[^\s]+$');
+			if(text.includes(" "))
+			{
+				return "name may not contain spaces";
+			}
+			else
+			{
+				return null;
+			}
+		}
+
+	});
+
+
+	const remote_local = await vscode.window.showQuickPick(
+							['local','remote'],
+							{placeHolder: 'Select if builder will be local or remote'});
+
+
+	let remote_host;
+	if(remote_local === undefined)
+	{
+		vscode.window.showErrorMessage('got no local / remote selection');
+		return;
+	}
+	if(remote_local === 'remote')
+	{
+		vscode.window.showWarningMessage("Remote not yet supported :(");
+	}
+
+	const buildroot = await vscode.window.showInputBox({
+		value: "enter the buildroot",
+		placeHolder: "/var/tmp/",
+		validateInput: text => {
+			if(text === "")
+			{
+				return "path may not be empty";
+			}
+			else if(text === "/")
+			{
+				return "this seems to be a realy bad idea";
+			}
+			else if(text.includes(" "))
+			{
+				return "path may not contain spaces";
+			}
+			else
+			{
+				return null;
+			}
+		}
+	});
+
+	context.globalState.update("builder_name", builder_name);
+	context.globalState.update("buildroot", buildroot);
+
+}
 // this method is called when your extension is deactivated
 export function deactivate() {}
